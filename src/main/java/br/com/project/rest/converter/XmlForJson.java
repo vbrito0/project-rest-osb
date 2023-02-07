5c4cc438e880389c9ff788b0b45cbbfa43cc85a0
Rafael <rlgonzalez@apoioprodesp.sp.gov.br>
2023-02-07 12:08:44 -0300

    Refatoração pessoa para cliente

--- src/main/java/br/com/project/rest/converter/XmlForJson.java
+++ src/main/java/br/com/project/rest/converter/XmlForJson.java
@@ -9,33 +9,33 @@ public class XmlForJson {
 public static void main(String[] args) throws JSONException {
 		
 		//--------------------------------------
-		// (1) monta "pessoa.xml" em uma string
+		// (1) monta "cliente.xml" em uma string
 		//--------------------------------------
 				
 		String xmlString = "<?xml version=\"1.0\"?>" + 
-					"<pessoas>" + 
-						"<pessoa>" +
+					"<clientes>" + 
+						"<cliente>" +
 							"<nome>Victor</nome>" + 
 							"<idade>20</idade>" + 
-						"</pessoa>" + 
-						"<pessoa>" + 
+						"</cliente>" + 
+						"<cliente>" + 
 							"<nome>Leonardo</nome>" + 
 							"<idade>16</idade>" + 
-						"</pessoa>" + 
-					"</pessoas>";
+						"</cliente>" + 
+					"</clientes>";
 		
 		//--------------------------------------------------------------
 		// (2) cria um JSONObject a partir do arquivo XML
 		//     com o uso do método estático "toJSONObject" da classe XML
 		//---------------------------------------------------------------
 		
-		JSONObject pessoasJson = XML.toJSONObject(xmlString);
+		JSONObject clientesJson = XML.toJSONObject(xmlString);
 		
 		//----------------------------------------------------------------
 		// (3) gera e imprime a string JSON, com o uso do método toString() 
 		//-----------------------------------------------------------------
 		
-		System.out.println(pessoasJson.toString());
+		System.out.println(clientesJson.toString());
 		
 	}
 }
